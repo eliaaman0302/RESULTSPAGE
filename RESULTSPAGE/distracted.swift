76dd8dbcf253f4452eaa52080abd84b245789f1e
Elina <elinaaman0302@gmail.com>
2024-08-01 15:06:36 +0100

    results with all the colors

--- RESULTSPAGE/distracted.swift
+++ RESULTSPAGE/distracted.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct distracted: View {
     var body: some View {
         ZStack {
-            Color("myPurple")
+            Color("myGreen")
                 .ignoresSafeArea()
            
             VStack {
