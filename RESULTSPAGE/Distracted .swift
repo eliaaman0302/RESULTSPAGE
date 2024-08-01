94ebca9c43109e9e8fe7781408adff1022ac5ecb
Elina <elinaaman0302@gmail.com>
2024-08-01 13:31:46 +0100

    3 result pages

---
+++ RESULTSPAGE/Distracted .swift
@@ -0,0 +1,18 @@
+//
+//  Distracted .swift
+//  RESULTSPAGE
+//
+//  Created by Scholar on 01/08/2024.
+//
+
+import SwiftUI
+
+struct Distracted_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Distracted_()
+}
---
+++ RESULTSPAGE/distracted.swift
@@ -0,0 +1,18 @@
+//
+//  distracted.swift
+//  RESULTSPAGE
+//
+//  Created by Scholar on 01/08/2024.
+//
+
+import SwiftUI
+
+struct distracted: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    distracted()
+}
