3e861a38a2e1dfa63bb6eee03d87fe4a1d00c54d
Elina <elinaaman0302@gmail.com>
2024-08-01 09:50:22 +0100

    updates page

--- RESULTSPAGE/ContentView.swift
+++ RESULTSPAGE/ContentView.swift
@@ -9,16 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color("myPurple")
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Perfectionist procrastinator ")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                Image("cat11")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    
+                VStack {
+                    Text("Typical Behaviours")
+                        .fontWeight(.heavy)
+                        .padding(.top)
+                    Text("• you know what and how to do it but your thoughts pull you back.")
+                        .padding(.horizontal)
+                    Text("• you spend hours on the beginning")
+                        .padding(.horizontal)
+                        .padding(.trailing, 55.0)
+                    Text("• you keep wanting to add 'one more change'")
+                        .padding(.horizontal)
+                        .padding(.trailing, 87.0)
+                        .padding(.bottom)
+                    
+                } .background(Rectangle()
+                    .foregroundColor(.white))
+                .cornerRadius(10)
+                .padding()
+                
+                VStack {
+                    Text("Solutions")
+                        .fontWeight(.heavy)
+                        .padding(.top)
+                    Text("• Shift your focus from results to actions until you get used to your routine")
+                        .padding(.horizontal)
+                    Text("• Set realistic time limits for different sections of your work")
+                        .padding(.horizontal)
+                    Text("• Keep you tasks bite-sized rather than an all-or nothing mentality").padding(.horizontal)
+                    Text("• Consciously lower your expectations, free yourself from believing you have to give 100% to everything you do.")
+                        .padding(.horizontal)
+                        .padding(.bottom)
+                }.background(Rectangle()
+                    .foregroundColor(.white))
+                .cornerRadius(10)
+                .padding()
+
+                
+                
+                
+            }
         }
-        .padding()
     }
 }
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
---
+++ RESULTSPAGE/dreamer.swift
@@ -0,0 +1,18 @@
+//
+//  dreamer.swift
+//  RESULTSPAGE
+//
+//  Created by Scholar on 01/08/2024.
+//
+
+import SwiftUI
+
+struct dreamer: View {
+    var body: some View {
+       
+    }
+}
+
+#Preview {
+    dreamer()
+}
