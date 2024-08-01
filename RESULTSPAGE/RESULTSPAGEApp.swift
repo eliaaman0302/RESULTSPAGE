56224eeb994de89413233ebe49f235a40eecbae8
Elina <elinaaman0302@gmail.com>
2024-08-01 09:15:54 +0100

    Initial Commit

---
+++ RESULTSPAGE/RESULTSPAGEApp.swift
@@ -0,0 +1,17 @@
+//
+//  RESULTSPAGEApp.swift
+//  RESULTSPAGE
+//
+//  Created by Scholar on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RESULTSPAGEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
